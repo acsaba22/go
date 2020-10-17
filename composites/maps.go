3b9b2d809f88b3ad4fb8e6c8aa1a7b3196d50839
Csaba Andras <acsaba@gmail.com>
2020-10-17 22:09:58 +0200

    tips for maps

--- composites/maps.go
+++ composites/maps.go
@@ -8,9 +8,12 @@ import (
 // Every output line should contains a key and a value separated by a space.
 // The lines are sorted by keys.
 func PrintSorted(m map[string]string, out io.Writer) {
+	// Tips: create a key list, and use sort.Strings
+	// fmt.Fprintf takes as first parameter an io.Writer
 }
 
 // Eq returns true if the two maps are equal.
 func Eq(x, y map[string]int) bool {
+	// Iterate over one check values in the other.
 	return false
 }
