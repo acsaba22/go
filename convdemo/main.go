7ecb4890064fb4c6f7be8960005eea321aae4b31
Csaba Andras <acsaba@gmail.com>
2020-10-17 19:45:31 +0200

    remove link to training module

--- convdemo/main.go
+++ convdemo/main.go
@@ -1,14 +1,8 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/acsaba22/go/conv"
-)
-
 func main() {
-	h := conv.Meter(1.92) // Zurich - Stuttgart
-	fmt.Printf("type: %T %v\n", h, h)
-	hf := conv.MeterToFoot(h)
-	fmt.Printf("type: %T %v\n", hf, hf)
+	// h := conv.Meter(5)
+	// fmt.Printf("type: %T %v\n", h, h)
+	// hf := conv.MeterToFoot(h)
+	// fmt.Printf("type: %T %v\n", hf, hf)
 }
